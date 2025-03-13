5544120089df54f574d2418de4c50428fe7e4ba0
KrishanMihiranga <xp3cto95@gmail.com>
2025-03-13 22:09:56 +0530

    refactor: create product

--- src/stock/stock.service.ts
+++ src/stock/stock.service.ts
@@ -7,6 +7,8 @@ import errors from 'src/config/errors.config';
 import slugify from 'slugify';
 import { ProductColors } from 'src/colors/schemas/colors.schema';
 import { ProductSize } from 'src/size/schemas/size.schema';
+import { Categories } from 'src/categories/schemas/categories.schema';
+import { SubCategories } from 'src/categories/schemas/sub-categories.schema';
 
 @Injectable()
 export class StockService {
@@ -16,63 +18,79 @@ export class StockService {
         @InjectModel(Stock.name) private StockModel: Model<Stock>,
         @InjectModel(ProductColors.name) private ProductColorsModel: Model<ProductColors>,
         @InjectModel(ProductSize.name) private ProductSizesModel: Model<ProductSize>,
+        @InjectModel(Categories.name) private CategoriesModel: Model<Categories>,
+        @InjectModel(SubCategories.name) private SubCategoriesModel: Model<SubCategories>,
     ) { }
     async getAll() {
         const stocks = await this.StockModel.find().select('-_id');
 
-        const stocksWithColors = await Promise.all(stocks.map(async (stock) => {
-            const updatedImages = await Promise.all(stock.Images.map(async (image) => {
-                const color = await this.ProductColorsModel.findOne({ key: image.colorKey }).exec();
-                if (color) {
-                    image.colorKey = color;
-                }
-                return image;
-            }));
+        const stocksWithColors = await Promise.all(
+            stocks.map(async (stock) => {
+                const updatedProductDetails = await Promise.all(
+                    stock.ProductDetails.map(async (detail) => {
+                        const color = await this.ProductColorsModel.findOne({ key: detail.colorKey }).exec();
+                        if (color) {
+                            detail.colorKey = color;
+                        }
+                        return detail;
+                    })
+                );
 
-            return {
-                ...stock.toObject(),
-                Images: updatedImages,
-            };
-        }));
+                return {
+                    ...stock.toObject(),
+                    ProductDetails: updatedProductDetails,
+                };
+            })
+        );
 
         return stocksWithColors;
     }
 
-
     async create(product: StockDto) {
-        if (product.Images.length === 0) {
+        if (!product.ProductDetails?.length) {
             throw new BadRequestException(errors.productImagesRequired);
         }
-        if (!product.Price || product.Price <= 0) {
-            throw new BadRequestException(errors.productPriceRequired);
-        }
-        if (!product.Sizes || product.Sizes.length === 0) {
+        if (!product.ProductDetails.some(detail => detail.Sizes?.length)) {
             throw new BadRequestException(errors.productSizesRequired);
         }
-        if (!product.Description || product.Description.trim() === '') {
+        if (!product.Description?.trim()) {
             throw new BadRequestException(errors.productDescriptionRequired);
         }
 
-        const colorKeys = product.Images.map(image => image.colorKey);
+
+        const colorKeys = product.ProductDetails.map(detail => detail.colorKey);
         const existingColors = await this.ProductColorsModel.find({ key: { $in: colorKeys } }).exec();
-        const existingColorKeys = existingColors.map(color => color.key);
-        const missingColorKeys = colorKeys.filter(colorKey => !existingColorKeys.includes(colorKey));
-        if (missingColorKeys.length > 0) {
+        const missingColorKeys = colorKeys.filter(colorKey => !existingColors.some(color => color.key === colorKey));
+        if (missingColorKeys.length) {
             throw new BadRequestException(errors.colorNotFound);
         }
 
-        const sizes = product.Sizes.map(size => size);
+
+        const sizes = product.ProductDetails.flatMap(detail => detail.Sizes);
         const existingSizes = await this.ProductSizesModel.find({ size: { $in: sizes } }).exec();
-        const existingSizesKeys = existingSizes.map(size => size.size);
-        const missingSizes = sizes.filter(sizeKey => !existingSizesKeys.includes(sizeKey));
-        if (missingSizes.length > 0) {
+        const missingSizes = sizes.filter(size => !existingSizes.some(existing => existing.size === size));
+        if (missingSizes.length) {
             throw new BadRequestException(errors.sizeNotFound);
+        }
+
+        const category = product.mainCategoryKey;
+        const existingCategory = await this.CategoriesModel.findOne({ key: category }).exec();
+
+        if (!existingCategory) {
+            throw new BadRequestException(errors.categoryNotFound);
+        }
+        const subCategory = product.subCategoryKey;
+        const existingSubCategory = await this.SubCategoriesModel.findOne({ key: subCategory }).exec();
+
+        if (!existingSubCategory) {
+            throw new BadRequestException(errors.categoryNotFound);
         }
 
 
         let baseSlug = slugify(product.name, { lower: true, strict: true });
         let slug = baseSlug;
         let count = 1;
+
 
         while (await this.StockModel.exists({ slug })) {
             slug = `${baseSlug}-${count}`;
@@ -86,6 +104,7 @@ export class StockService {
 
         return newProduct.save();
     }
+
 
     async delete(slug: string) {
         const result = await this.StockModel.deleteOne({ slug }).exec();
